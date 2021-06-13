54c0a77906452a7e08bf1977a95d6e238f9f9617
Lucien Käser <lucien.kaeser@me.com>
2021-06-13 18:54:05 +0200

    Added solution on db

--- EinsteinHacking.DB/Data/Migrations/20210613165122_SolutionOnChallenge.Designer.cs
+++ EinsteinHacking.DB/Data/Migrations/20210613165122_SolutionOnChallenge.Designer.cs
@@ -4,14 +4,16 @@ using EinsteinHacking.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EinsteinHacking.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210613165122_SolutionOnChallenge")]
+    partial class SolutionOnChallenge
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
