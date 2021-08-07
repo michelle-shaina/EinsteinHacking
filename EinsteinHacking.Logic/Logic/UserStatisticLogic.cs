ab0738df1bfa8e67f7111e0d9a9817acacb6b9a0
Lucien Käser <lucien.kaeser@me.com>
2021-08-07 17:29:02 +0200

    Fixed bug that causes the application to stop running, when user logs in for the first time

--- EinsteinHacking.Logic/Logic/UserStatisticLogic.cs
+++ EinsteinHacking.Logic/Logic/UserStatisticLogic.cs
@@ -20,22 +20,30 @@ namespace EinsteinHacking.Logic
         /// <returns></returns>
         public int GetUserScore(string userID)
         {
+
             int score = 0;
-            if (String.IsNullOrEmpty(userID)) return 0;
-            var userProgress = _context.UserInformation.Include("Progress")
-                .FirstOrDefault(n => n.User.NormalizedUserName == userID.ToUpper())?.Progress;
-            foreach (var progress in userProgress)
+            try
             {
-                if(progress.Status == Models.Status.Ended)
+                if (String.IsNullOrEmpty(userID)) return 0;
+                var userProgress = _context.UserInformation.Include("Progress")
+                    .FirstOrDefault(n => n.User.NormalizedUserName == userID.ToUpper())?.Progress;
+                foreach (var progress in userProgress)
                 {
-                    var targetChallenge = _context.UserProgress.Include("Challenge")
-                        .FirstOrDefault(n => n.UserProgressID == progress.UserProgressID)?.Challenge;
-                    if(targetChallenge != null)
+                    if (progress.Status == Models.Status.Ended)
                     {
-                        score += targetChallenge.PointsOnCompletion;
-                        score -= (targetChallenge.PointsRemovedPerHintUsed * progress.HintsUsed);
+                        var targetChallenge = _context.UserProgress.Include("Challenge")
+                            .FirstOrDefault(n => n.UserProgressID == progress.UserProgressID)?.Challenge;
+                        if (targetChallenge != null)
+                        {
+                            score += targetChallenge.PointsOnCompletion;
+                            score -= (targetChallenge.PointsRemovedPerHintUsed * progress.HintsUsed);
+                        }
                     }
                 }
+            }
+            catch
+            {
+
             }
             return score;
         }
